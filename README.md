9376c0932a9cc746e507deaba8406163b753054b
John White <jhwhite@ithaca.edu>
2019-07-10 09:59:26 -0400

    Browser-sync working.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ithaca Sakai Skin
+
+Inspired by:
+
+* [Duke Sakai Skins](https://github.com/DukeLearningInnovation/duke-sakai-skins)
+* [Sakai Morpheus](https://github.com/sakaiproject/sakai/tree/master/library)
+
+## Work in progress
+
+### Known issues
+
+* [ ] Icons not working
+* [ ] (so much more...)
+* [ ] Need a script to package up theme files
+
+### How to use
+
+To run local browser-sync for development:
+
+```bash
+yarn install
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const gulp        = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass'),
+    reload      = browserSync.reload,
+    src = {
+        scss: 'src/morpheus-master/sass/*.scss',
+        css:  'examples/css',
+        html: 'examples/*.html'
+    };
+
+// Static Server + watching scss/html files
+gulp.task('serve', ()=>{
+    browserSync.init({
+        server: {
+            baseDir: ["examples", "dist"],
+            directory: true
+        }
+    });
+    gulp.watch(src.scss, gulp.series(['sassTask']));
+    gulp.watch(src.html).on('change', reload);
+});
+
+// Compile sass into CSS
+gulp.task('sassTask', ()=>{
+    return gulp.src(src.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('default', gulp.parallel('sassTask', 'serve'));
